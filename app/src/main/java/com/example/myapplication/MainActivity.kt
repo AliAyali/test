397a564e6ef93c94815f9b4c5b352902e0ebfb45
AliAyali <ali.ayali.programmer@gmail.com>
2025-05-18 11:50:38 -0700

    create binding library

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.root.setOnClickListener {
+            Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
